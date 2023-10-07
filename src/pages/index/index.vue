bf6585a605b42de7dca0a33ba46a08fc9ec5a4f3
romejiang <romejiang@gmail.com>
2023-10-07 18:56:49 +0800

    feat: add unocss library

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,8 +1,8 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png" />
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
+  <view class="flex flex-col justify-center items-center">
+    <image class="m-10 w-30 h-30 rounded-full" src="/static/logo.png" />
+    <view class="text-center">
+      <text class="text-2xl">{{ title }}</text>
     </view>
   </view>
 </template>
@@ -12,30 +12,4 @@ import { ref } from 'vue'
 const title = ref('Hello')
 </script>
 
-<style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
-}
-</style>
+<style></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
+import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni()],
+  plugins: [uni(),UnoCSS()],
 });
